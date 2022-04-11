d9e4607df2f1e42f16540568c73ece626352d001
ayarmkt <ayat.0906@gmail.com>
2022-04-11 18:02:38 +0200

    added modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import './App.css';
 import MainPage from './components/MainPage';
+import TaskItemDetailModal from './components/TaskItemDetailModal';
 
 function App() {
-  return <MainPage />;
+  return (
+    <React.Fragment>
+      <MainPage />
+      <TaskItemDetailModal />
+    </React.Fragment>
+  );
 }
 
 export default App;
