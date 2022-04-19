53ed6b3eb041be0e5a31aae1eca19a2e7325fbf8
ayarmkt <ayat.0906@gmail.com>
2022-04-19 11:39:35 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import './App.css';
-import MainPage from './components/MainPage';
-import TaskItemDetailModal from './components/TaskItemDetailModal';
-import { useSelector } from 'react-redux';
 
 function App() {
-  const { modalOpen } = useSelector((state) => state.ui);
-
-  return (
-    <React.Fragment>
-      <MainPage />
-      {modalOpen && <TaskItemDetailModal />}
-    </React.Fragment>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import uiReducer from './ui-slice';
-import taskReducer from './task-slice';
-
-const store = configureStore({ reducer: { ui: uiReducer, task: taskReducer } });
-
-export default store;
